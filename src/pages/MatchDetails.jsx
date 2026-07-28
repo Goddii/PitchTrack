bf580684d1cb5e119eafdffb2dd1e0fe4af6f91b
Goddii <otienogodwin23@gmail.com>
2026-07-28 19:40:39 +0300

    add import needed for page

---
+++ src/pages/MatchDetails.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+import { ArrowLeft, MapPin, Calendar, CalendarX } from "lucide-react"
+import Navbar from "../components/Navbar"
+import EmptyState from "../components/EmptyState"
+import { MOCK_MATCHES } from "../data/mockData"
