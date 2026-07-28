7c49069bda43bf0ac5ba65659d97d7210a86de31
Goddii <otienogodwin23@gmail.com>
2026-07-28 19:02:52 +0300

    add state management to page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,3 +3,14 @@ import { Link, useNavigate } from "react-router-dom";
 import { User, Mail, Lock, AlertCircle } from "lucide-react";
 import Navbar from "../components/Navbar";
 import { useAuth } from "../context/AuthContext"
+
+export default function Register() {
+    const { register } = useAuth();
+    const navigate = useNavigate();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    const [submitting, setSubmitting] = useState(false);
