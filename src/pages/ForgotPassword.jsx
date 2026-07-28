fff4c1d2e6cf4fadde0b6e27674f9ac68a8aa4e9
Goddii <otienogodwin23@gmail.com>
2026-07-28 18:43:21 +0300

    add imports required for page

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Mail, AlertCircle, CheckCircle2 } from "lucide-react";
+import Navbar from "../components/Navbar";
+import api from "../services/api";
