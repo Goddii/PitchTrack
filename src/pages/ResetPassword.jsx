b8030e70b08eddf38758fd06765a84195e699c4f
Goddii <otienogodwin23@gmail.com>
2026-07-28 18:51:59 +0300

    add required imports

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { Lock, AlertCircle, CheckCircle2 } from "lucide-react";
+import Navbar from "../components/Navbar";
+import api from "../services/api";
+
